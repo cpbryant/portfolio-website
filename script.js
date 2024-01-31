3af368e78199547251fbf6c0e25d72b71be3d018
cpbryant <113325662+cpbryant@users.noreply.github.com>
2024-01-31 00:21:56 -0500

    Everything but demos complete

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Target the two elements and either add or remove the class */
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
